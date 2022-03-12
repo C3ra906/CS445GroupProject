43e87436c94a97da71b5812d4f702afac16b9a3e
Michael Budei <mbudei@pdx.edu>
2022-03-12 13:28:40 -0800

    Updated MPL file

--- mlp/mlp.py
+++ mlp/mlp.py
@@ -10,10 +10,17 @@ class MLP:
         self.learning_rate = learning_rate
         self.momentum = momentum
         self.num_of_hl = hidden_layer_count
+        row, columns = self.train_data.shape
+        self.hidden_layer_weights = self.generate_random_weights(hidden_layer_count)
 
     def setup_data(self, file_path):
         file = np.loadtxt(open(file_path, "rb"), delimiter=",", skiprows=1)
-        row, column = file.shape
-        self.test_data, self.train_data = train_test_split(file, file[:, len(column)])
+        self.train_data, self.test_data = train_test_split(file)
+        print(self.test_data)
 
-test1 = MLP('./numerical_data.csv', 0.1, 2, 20)
+    @staticmethod
+    def generate_random_weights(row, col):
+        matrix = np.random.uniform(-0.05, 0.05, size=(row, col))
+        return matrix
+
+test1 = MLP('./normalized_data.csv', 0.1, 2, 20)
